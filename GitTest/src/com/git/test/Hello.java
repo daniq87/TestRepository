64a0d00e9c5042c3ae170f4a4e28def110e78f32
alejandro <alejo@xmlconnectravel.com>
2016-01-26 11:49:30 +0100

    “prueba”

--- GitTest/src/com/git/test/Hello.java
+++ GitTest/src/com/git/test/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("NUEVA CLASE HELLO");
+		System.out.println("NUEVA CLASE Alejo");
 	}
 
 }
